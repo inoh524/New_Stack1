d59da617ad7884b9d4e1d7ae59d5eb23cf09c54a
inoh524 <inohsuu@gmail.com>
2026-07-03 13:01:37 +0800

    axios localhost

--- task-manager/frontend/src/api/axios.js
+++ task-manager/frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.1.49:3000/api",
+  baseURL: "http://localhost:3000/api",
+  // baseURL: "http://192.168.1.49:3000/api", //danielle's house IP address
+  //  baseURL: "http://10.0.7.117:3000/api", //Jeonsoft house IP address
   headers: {
     "Content-Type": "application/json",
   },
