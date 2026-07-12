9dea76ac8c7e3cc8326697e7d37867fcb51315b2
inoh524 <inohsuu@gmail.com>
2026-07-12 11:33:06 +0800

    axios up

--- task-manager/frontend/src/api/axios.js
+++ task-manager/frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
+  // baseURL: "http://localhost:3000/api",
   // baseURL: "http://192.168.1.49:3000/api", //danielle's house IP address
   //  baseURL: "http://10.0.7.117:3000/api", //Jeonsoft house IP address
   headers: {
