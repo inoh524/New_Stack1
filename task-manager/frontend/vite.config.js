a29329659484a62611c3de077593f6c34d09545e
inoh524 <inohsuu@gmail.com>
2026-07-12 09:28:26 +0800

    vite config

--- task-manager/frontend/vite.config.js
+++ task-manager/frontend/vite.config.js
@@ -9,6 +9,7 @@ const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  BASE: process.env.VITE_BASE_PATH || "/task-manager/frontend",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
