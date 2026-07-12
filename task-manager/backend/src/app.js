3ad9b826adfccbee8a06833cd8f5ae90dfd0e8b4
inoh524 <inohsuu@gmail.com>
2026-07-12 11:59:11 +0800

    backend status

--- task-manager/backend/src/app.js
+++ task-manager/backend/src/app.js
@@ -3,14 +3,16 @@ const cors = require("cors");
 
 const user_routes = require("./routes/user_routes");
 const auth_routes = require("./routes/auth_routes");
+
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
-app.use("/users", user_routes);
-app.use("/auth", auth_routes);
+app.get("/", (req, res) => {
+  res.send("Backend API is running");
+});
+
 app.use("/api/users", user_routes);
 app.use("/api/auth", auth_routes);
 
