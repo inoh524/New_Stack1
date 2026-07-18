5462f4c4f6a08bd8de6dfed0c47f4b3aa62daac3
inoh524 <inohsuu@gmail.com>
2026-07-18 13:41:30 +0800

    render api

--- task-manager/frontend/src/api/axios.js
+++ task-manager/frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://new-stack1.onrender.com/api", // Replace with your render API URL
-  baseURL: "http://localhost:3000/api", // Replace with your local API URL
+  baseURL: "https://new-stack1.onrender.com/api", // Replace with your render API URL
+  // baseURL: "http://localhost:3000/api", // Replace with your local API URL
   headers: {
     "Content-Type": "application/json",
   },
